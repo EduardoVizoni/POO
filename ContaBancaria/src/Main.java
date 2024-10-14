34792b589b66bc1e3298dfa230e01f45c1abb63a
Eduardo Vizoni do Prado <145796079+EduardoVizoni@users.noreply.github.com>
2024-10-14 16:03:44 -0300

    .

--- ContaBancaria/src/Main.java
+++ ContaBancaria/src/Main.java
@@ -3,19 +3,49 @@
 public class Main {
     public static void main(String[] args) {
 
-        Conta conta1 = new Conta(1, "Romário", 80000, 50000);
-        Conta conta2 = new Conta(2, "Kristian", 54000, 30000);
 
-        try {
-            conta1.saque(90000000);
-        } catch (SaldoInsuficienteException  exception) {
-            System.out.println(exception.getMessage());
-        } catch (LimiteInsuficienteException | ValorInvalidoException exception) {
-            exception.printStackTrace(); // O método printStackTrace() serve para exibir o conteúdo da pilha de execução quando ocorre uma exceção.
-        } catch (ContaException ignore) {}
+    }
 
+    private void cadastroConta() {
 
-        System.out.println("Ainda está rodando");
+    }
 
+    private void removeConta() {
+
+    }
+
+    private void editaConta() {
+
+    }
+
+    private void buscaConta() {
+
+    }
+
+    private void login() {
+
+    }
+
+    private void logout() {
+
+    }
+
+    private void listarConta() {
+
+    }
+
+    private void mostrarDadosConta() {
+
+    }
+
+    private void dadosTitular() {
+
+    }
+
+    private void mostrarOpcoes() {
+
+    }
+
+    private void executarOpcao() {
     }
 }
